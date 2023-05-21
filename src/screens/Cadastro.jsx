9ec63390a39fd77975c61864ee8c516c420abf11
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-21 16:36:39 -0300

    feat: create signup screen

---
+++ src/screens/Cadastro.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+
+export default function Cadastro() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  async function handleSubmit() {
+    const response = await fetch('http://localhost:3000/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        name,
+      }),
+    });
+
+    if (!response.ok) {
+      alert('Error signing up.');
+    } else {
+      alert('Signed up successfully!');
+      // Navegue para a próxima tela aqui.
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Sign Up" onPress={handleSubmit} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingLeft: 10,
+  },
+});
