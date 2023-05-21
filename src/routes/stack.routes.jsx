94697cabdd41b959b739e7a1f92d15fcc01f601c
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-21 16:36:32 -0300

    feat: using screens in stack routes

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,5 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "../screens/Home";
+import { Login } from "../screens/Login";
+import OnboardingScreen from "../screens/OnboardingScreen";
+import Cadastro from "../screens/Cadastro";
 
 
 const { Screen, Navigator } = createNativeStackNavigator();
@@ -7,6 +10,23 @@ const { Screen, Navigator } = createNativeStackNavigator();
 export function StackRoutes(){
   return (
     <Navigator>
+      <Screen
+        name="Onboarding"
+        component={OnboardingScreen}
+        options={
+          {
+            headerShown: false
+          }
+        }
+      />
+      <Screen
+        name="Login"
+        component={Login}
+      />
+      <Screen
+        name="Cadastro"
+        component={Cadastro}
+      />
       <Screen 
         name="Home" 
         component={Home} 
