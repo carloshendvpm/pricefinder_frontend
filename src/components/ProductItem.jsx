4a50c95e5ea4668cab5a71e13d5392954979ab65
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-25 15:19:45 -0300

    feat: create product item component

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Card, Avatar, IconButton } from 'react-native-paper'
+
+
+export function ProductItem({ product }) {
+  return (
+    <View >
+      <Card.Title
+          key={product.id}
+          title={product.name}
+          subtitle={'R$ ' + 23.90}
+          left={(props) => <Avatar.Icon {...props} icon="cart" />}
+          right={(props) => <IconButton {...props} icon="chevron-right" />}
+        />
+    </View>
+  );
+}
