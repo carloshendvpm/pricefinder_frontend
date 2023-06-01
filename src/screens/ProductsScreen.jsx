4befe64c68c20df5733877393f9a70c6d43c2dee
pedrochalegre7 <pedrochalegre7@gmail.com>
2023-05-31 21:55:39 -0300

    feat: added search bar on products screen. still needs to be tested

--- src/screens/ProductsScreen.jsx
+++ src/screens/ProductsScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, TextInput, FlatList, StyleSheet } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { Button, Text } from 'react-native-paper';
 import { ProductItem } from '../components/ProductItem';
@@ -9,6 +9,8 @@ export function ProductsScreen({ navigation }) {
   const route = useRoute();
   const { market_id } = route.params;
   const [products, setProducts] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   useFocusEffect(
     useCallback(() => {
@@ -23,21 +25,76 @@ export function ProductsScreen({ navigation }) {
     }, [])
   );
 
+  const handleSearch = () => {
+    if (searchText.trim() !== '') {
+      const results = products.filter(product =>
+        product.name.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setSearchResults(results);
+    } else {
+      setSearchResults([]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Button mode='contained-tonal' onPress={() => navigation.navigate('CadastraProduto', { market_id })} styles={styles.button}>Adicionar novo produto</Button>
-      {products.length > 0 
-        ? products.map((product) => (
-            <ProductItem key={product.id} product={product} />
-          ))
-        : <Text>Ops... Parece que você não tem produtos cadastrados</Text>
-      }
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Pesquisar produtos"
+          onChangeText={text => setSearchText(text)}
+          value={searchText}
+        />
+        <Button onPress={handleSearch} mode='contained-tonal' style={styles.searchButton}>Pesquisar</Button>
+      </View>
+      <Button mode='contained-tonal' onPress={() => navigation.navigate('CadastraProduto', { market_id })} style={styles.button}>Adicionar novo produto</Button>
+      {searchText.trim() !== '' && searchResults.length > 0 ? (
+        <FlatList
+          data={searchResults}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <ProductItem key={item.id} product={item} />
+          )}
+        />
+      ) : (
+        <FlatList
+          data={products}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <ProductItem key={item.id} product={item} />
+          )}
+        />
+      )}
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     paddingHorizontal: 10,
-    paddingVertical:10
+    paddingVertical: 10
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 18,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+  searchButton: {
+    marginLeft: 10,
+  },
+  button: {
+    marginTop: 10,
   },
 });
