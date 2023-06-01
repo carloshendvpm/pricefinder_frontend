88ff032cdf0d0f2bf8490f57c108677465747647
Carlos Henrique <hencarlosdv@gmail.com>
2023-06-01 17:36:01 -0300

    feat: add product details

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Card, Avatar, IconButton } from 'react-native-paper'
-
+import { useNavigation } from '@react-navigation/native';
 
 export function ProductItem({ product }) {
+  const navigation = useNavigation();
   return (
-    <View >
-      <Card.Title
-          key={product.id}
-          title={product.name}
-          titleStyle={{fontSize: 16, fontWeight: 'bold'}}
-          subtitle={'R$' + product.price}
-          left={(props) => <Avatar.Icon {...props} icon="cart" />}
-          right={(props) => <IconButton {...props} icon="chevron-right" />}
-        />
-    </View> 
+    <TouchableOpacity onPress={() => navigation.navigate('ProductDetails', { product })}>
+      <View>
+        <Card.Title
+            key={product.id}
+            title={product.name}
+            titleStyle={{fontSize: 16, fontWeight: 'bold'}}
+            subtitle={'R$' + product.price}
+            left={(props) => <Avatar.Icon {...props} icon="cart" />}
+            right={(props) => <IconButton {...props} icon="chevron-right" />}
+          />
+      </View> 
+    </TouchableOpacity>
   );
 }
